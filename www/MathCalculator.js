66bd9bc4a8eb208c99772990fee7ccca5697a7a9
Okan PEHLİVAN <okan.pehlivan@wunder.is>
2022-10-14 12:00:11 +0300

    location code removed

--- www/MathCalculator.js
+++ www/MathCalculator.js
@@ -7,7 +7,3 @@ module.exports.add = function (arg0, success, error) {
 module.exports.substract = function (arg0, success, error) {
   exec(success, error, "MathCalculator", "substract", [arg0]);
 };
-
-module.exports.getLocationPermission = function (arg0, success, error) {
-  exec(success, error, "MathCalculator", "getLocationPermission", [arg0]);
-};
