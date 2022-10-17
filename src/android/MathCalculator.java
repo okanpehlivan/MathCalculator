eeec476e789017a35dbf567afe633314938351b2
Okan PEHLİVAN <okan.pehlivan@wunder.is>
2022-10-17 15:07:21 +0300

    location

--- src/android/MathCalculator.java
+++ src/android/MathCalculator.java
@@ -2,14 +2,12 @@ package cordova.plugin.mathcalculator;
 
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
+import java.util.Locale;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-/**
- * This class echoes a string called from JavaScript.
- */
 public class MathCalculator extends CordovaPlugin {
 
     @Override
@@ -22,8 +20,51 @@ public class MathCalculator extends CordovaPlugin {
             this.substract(args, callbackContext);
 
             return true;
+        } else if (action.equals("getLocationPermission")) {
+            this.getLocationPermission(args, callbackContext);
+
+            return true;
         }
         return false;
+    }
+
+    private void getLocationPermission(String action, CallbackContext callback) {
+        try {
+            String result = Locale.getDefault().getCountry();
+            callback.success(""+ result );
+        } catch (Exception ex) {
+            callback.error("Location permission error");
+        }
+
+        // try {
+        //     ActivityResultLauncher<String[]> locationPermissionRequest =
+        //             registerForActivityResult(new ActivityResultContracts
+        //                             .RequestMultiplePermissions(), result -> {
+        //                         Boolean fineLocationGranted = result.getOrDefault(
+        //                                 Manifest.permission.ACCESS_FINE_LOCATION, false);
+        //                         Boolean coarseLocationGranted = result.getOrDefault(
+        //                                 Manifest.permission.ACCESS_COARSE_LOCATION,false);
+
+        //                         if (fineLocationGranted != null && fineLocationGranted) {
+        //                             // Precise location access granted.
+        //                             callback.success(true);
+        //                         } else if (coarseLocationGranted != null && coarseLocationGranted) {
+        //                             // Only approximate location access granted.
+        //                             callback.success(true);
+        //                         } else {
+        //                             // No location access granted.
+        //                             callback.success(false);
+        //                         }
+        //                     }
+        //             );
+
+        //     locationPermissionRequest.launch(new String[] {
+        //             Manifest.permission.ACCESS_FINE_LOCATION,
+        //             Manifest.permission.ACCESS_COARSE_LOCATION
+        //     });
+        // } catch (Exception ex) {
+        //     callback.error("Konum servisleri çalışırken bir hata verdi");
+        // }
     }
 
     private void add(JSONArray args, CallbackContext callback) {
