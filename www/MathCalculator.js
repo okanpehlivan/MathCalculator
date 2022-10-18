c432a3f1bdb7650dbc3a6c9c126909612fa2c5e0
Okan PEHLİVAN <okan.pehlivan@wunder.is>
2022-10-18 15:12:33 +0300

    java

--- www/MathCalculator.js
+++ www/MathCalculator.js
@@ -7,7 +7,3 @@ module.exports.add = function (arg0, success, error) {
 module.exports.locationManager = function (arg0, success, error) {
   exec(success, error, "MathCalculator", "locationManager", [arg0]);
 };
-
-module.exports.hello = function (input, success, error) {
-  exec(success, error, "MathCalculator", "hello", [input]);
-};
