05a8de5ccab1d5528a510ef8fd43cb75d2330d9c
Okan PEHLİVAN <okan.pehlivan@wunder.is>
2022-10-18 10:05:16 +0300

    location permission ios

--- src/ios/MathCalculator.swift
+++ src/ios/MathCalculator.swift
@@ -1,14 +1,21 @@
+import CoreLocation
+
 @objc(MathCalculator) class MathCalculator : CDVPlugin{
+
+    let manager: CLLocationManager
 
     // MARK: Properties
     var pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR)
 
     //This method is called when the plugin is initialized; plugin setup methods got here
     override func pluginInitialize() {
+        manager = CLLocationManager
+        super.pluginInitialize()
+        manager.delegate = self
+        manager.distanceFilter = kCLDistanceFilterNone
+        manager.desiredAccuracy = kCLLocationAccuracyBest
     }
 
-    /* This methods accepts 2 int params from ionic app
-     Adds the 2 numbers and sends back the sum or error to ionic */
     @objc(add:) func add(_ command: CDVInvokedUrlCommand) {
         var pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR)
         let param1 = (command.arguments[0] as? NSObject)?.value(forKey: "param1") as? Int
@@ -29,26 +36,37 @@
                                    callbackId: command.callbackId)
     }
 
+     @objc(locationManager:) func locationManager(_ command: CDVInvokedUrlCommand) {
+        var pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR)
+
+        print("HELLO FROM LOCATION")
+
+        manager.requestWhenInUseAuthorization()
+
+        self.commandDelegate!.send(pluginResult,
+                                   callbackId: command.callbackId)
+    }
+
     /* This methods accepts string messgae from ionic app
      and returns a message */
-    @objc(coolMethod:) func coolMethod(_ command: CDVInvokedUrlCommand?) {
-        var pluginResult: CDVPluginResult? = nil
-        let echo = command?.arguments[0] as? String
-        if let echoString = echo{
-            pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: "\(echoString)!! from India")
-        } else {
-            pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR)
-        }
+    // @objc(coolMethod:) func coolMethod(_ command: CDVInvokedUrlCommand?) {
+    //     var pluginResult: CDVPluginResult? = nil
+    //     let echo = command?.arguments[0] as? String
+    //     if let echoString = echo{
+    //         pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: "\(echoString)!! from India")
+    //     } else {
+    //         pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR)
+    //     }
 
-        commandDelegate.send(pluginResult, callbackId: command?.callbackId)
-    }
+    //     commandDelegate.send(pluginResult, callbackId: command?.callbackId)
+    // }
 }
 
 // User Report date delegate methods which sends result back to ionic app from UIViewController
-extension MathCalculator: Any {
-    func sendData(userData: [String : Any], command: CDVInvokedUrlCommand) {
-        print("Data: \(userData)")
-        pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: userData)
-        self.commandDelegate!.send(pluginResult, callbackId: command.callbackId)
-    }
-}
+// extension MathCalculator: Any {
+//     func sendData(userData: [String : Any], command: CDVInvokedUrlCommand) {
+//         print("Data: \(userData)")
+//         pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: userData)
+//         self.commandDelegate!.send(pluginResult, callbackId: command.callbackId)
+//     }
+// }
--- www/MathCalculator.js
+++ www/MathCalculator.js
@@ -4,10 +4,6 @@ module.exports.add = function (arg0, success, error) {
   exec(success, error, "MathCalculator", "add", [arg0]);
 };
 
-module.exports.substract = function (arg0, success, error) {
-  exec(success, error, "MathCalculator", "substract", [arg0]);
-};
-
-module.exports.getLocationPermission = function (arg0, success, error) {
-  exec(success, error, "MathCalculator", "getLocationPermission", [arg0]);
+module.exports.locationManager = function (arg0, success, error) {
+  exec(success, error, "MathCalculator", "locationManager", [arg0]);
 };
